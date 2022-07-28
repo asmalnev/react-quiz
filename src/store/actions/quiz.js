e9ada48562e2113a9104184ddb1d9601ce9ea6d7
asmalnev <asmalnev@gmail.com>
2022-07-28 18:02:32 +0300

    12-03

--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -1,7 +1,5 @@
 import axios from '../../axios/axios-quiz'
-import {FETCH_QUIZES_START} from './actionTypes'
-import {FETCH_QUIZES_SUCCESS} from './actionTypes'
-import {FETCH_QUIZES_ERROR} from './actionTypes'
+import {FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZES_ERROR, FETCH_QUIZ_SUCCESS} from './actionTypes'
 
 export function fetchQuizes() {
   return async dispatch => {
@@ -23,6 +21,28 @@ export function fetchQuizes() {
     } catch (e) {
       dispatch(fetchQuizesError(e))
     }
+  }
+}
+
+export function fetchQuizById(quizId) {
+  return async dispatch => {
+    dispatch(fetchQuizesStart())
+
+    try {
+      const response = await axios.get(`/quizes/${quizId}.json`)
+      const quiz = response.data
+
+      dispatch(fetchQuizSuccess(quiz))
+    } catch (e) {
+      dispatch(fetchQuizesError(e))
+    }
+  }
+}
+
+export function fetchQuizSuccess(quiz) {
+  return {
+    type: FETCH_QUIZ_SUCCESS,
+    quiz
   }
 }
 
